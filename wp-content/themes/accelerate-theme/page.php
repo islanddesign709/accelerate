b1c1395c142bafea8bd4f65fb79ffd0c3caff0f3
Susan <islanddesign709@gmail.com>
2016-02-09 23:40:47 -0330

    changeheader

--- wp-content/themes/accelerate-theme/page.php
+++ wp-content/themes/accelerate-theme/page.php
@@ -17,7 +17,7 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+				<?php the_excerpt(); ?>
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
